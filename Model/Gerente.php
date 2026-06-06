5b46f8a135eaa4b205b4fc8630368f53bf307359
Gabriel Pablo Garcia <gabrielpablo.g55@gmail.com>
2026-06-06 19:21:09 -0300

    fix(Gerente): trash legacy model

--- Model/Gerente.php
+++ Model/Gerente.php
@@ -1,89 +1,100 @@
 <?php
-	class Gerente {
-		private $nomeGerente;
-		private $cpfGerente;
-		private $sexoGerente;
-        private $NascGerente;
-        private $salarioGerente;
-        private $emailGerente;
-        private $senhaGerente;
 
-        private $rua;
-        private $bairro;
-        private $cep;
-        private $numero_casa;
-		
-		public function getNomeGerente(){
-            return $this->nomeGerente;
-        }
-        public function setNomeGerente($nomeGerente){
-            $this->nomeGerente = $nomeGerente;
-        }
-		
-		public function getCpfGerente(){
-			return $this->cpfGerente;
-		}
-		public function setCpfGerente($cpfGerente){
-			$this->cpfGerente = $cpfGerente;
-		}
-		
-		public function getSexoGerente(){
-			return $this->sexoGerente;
-		}
-		public function setSexoGerente($sexoGerente){
-			$this->sexoGerente = $sexoGerente;
-		}
-        
-        public function getNascGerente(){
-            return $this->NascGerente;
-        }
-        public function setNascGerente($NascGerente){
-            $this->NascGerente = $NascGerente;
-        }
-        
-        public function getSalarioGerente(){
-            return $this->salarioGerente;
-        }
-        public function setSalarioGerente($salarioGerente){
-            $this->salarioGerente = $salarioGerente;
-        }
-        
-        public function getEmailGerente() {
-            return $this->emailGerente;
-        }
-        public function setEmailGerente($emailGerente) {
-            $this->emailGerente = $emailGerente;
-        }
-        
-        public function getSenhaGerente() {
-            return $this->senhaGerente;
-        }
-        public function setSenhaGerente($senhaGerente) {
-            $this->senhaGerente = $senhaGerente;
-        }
-        
-        public function getRua() {
-            return $this->rua;
-        }
-        public function setRua($rua) {
-            $this->rua = $rua;
-        }
-        public function getBairro() {
-            return $this->bairro;
-        }
-        public function setBairro($bairro) {
-            $this->bairro = $bairro;
-        }
-        public function getCep() {
-            return $this->cep;
-        }
-        public function setCep($cep) {
-            $this->cep = $cep;
-        }
-        public function getNumeroCasa() {
-            return $this->numero_casa;
-        }
-        public function setNumeroCasa($numero_casa) {
-            $this->numero_casa = $numero_casa;
-        }
-	}
+class Gerente {
+    private $id;
+    private $nome;
+    private $cpf;
+    private $sexo;
+    private $nascimento;
+    private $salario;
+    private $email;
+    private $senha;
+    private $rua;
+    private $bairro;
+    private $cep;
+    private $numero_casa;
+
+    public function getId() {
+        return $this->id;
+    }
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getCpf() {
+        return $this->cpf;
+    }
+    public function setCpf($cpf) {
+        $this->cpf = $cpf;
+    }
+
+    public function getSexo() {
+        return $this->sexo;
+    }
+    public function setSexo($sexo) {
+        $this->sexo = $sexo;
+    }
+
+    public function getNascimento() {
+        return $this->nascimento;
+    }
+    public function setNascimento($nascimento) {
+        $this->nascimento = $nascimento;
+    }
+
+    public function getSalario() {
+        return $this->salario;
+    }
+    public function setSalario($salario) {
+        $this->salario = $salario;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getSenha() {
+        return $this->senha;
+    }
+    public function setSenha($senha) {
+        $this->senha = $senha;
+    }
+
+    public function getRua() {
+        return $this->rua;
+    }
+    public function setRua($rua) {
+        $this->rua = $rua;
+    }
+
+    public function getBairro() {
+        return $this->bairro;
+    }
+    public function setBairro($bairro) {
+        $this->bairro = $bairro;
+    }
+
+    public function getCep() {
+        return $this->cep;
+    }
+    public function setCep($cep) {
+        $this->cep = $cep;
+    }
+
+    public function getNumeroCasa() {
+        return $this->numero_casa;
+    }
+    public function setNumeroCasa($numero_casa) {
+        $this->numero_casa = $numero_casa;
+    }
+}

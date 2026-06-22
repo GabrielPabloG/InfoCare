b1039a44c3f833e57849b25357dca8e9f63bb67f
Gabriel Pablo Garcia <gabrielpablo.g55@gmail.com>
2026-06-21 23:50:08 -0300

    fix: remove diagnostic files and stabilize Railway deployment

--- testenv.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "MYSQLHOST: " . (getenv('MYSQLHOST') ?: 'NÃO DEFINIDO') . "<br>";
-echo "MYSQLPORT: " . (getenv('MYSQLPORT') ?: 'NÃO DEFINIDO') . "<br>";
-echo "MYSQLDATABASE: " . (getenv('MYSQLDATABASE') ?: 'NÃO DEFINIDO') . "<br>";
